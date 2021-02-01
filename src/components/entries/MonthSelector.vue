48a6ddd37f472712417a463d8bd8959572ca7fac
julian <30285755+juliangermek@users.noreply.github.com>
2021-02-01 23:13:01 +0100

    Create slider months dynamically

--- src/components/entries/MonthSelector.vue
+++ src/components/entries/MonthSelector.vue
@@ -1,28 +1,22 @@
 <template>
+  <ion-button @click="prevSlide" :disabled="disablePrevBtn">
+    <ion-icon slot="icon-only" :icon="chevronBack"></ion-icon>
+  </ion-button>
+
   <ion-slides
     ref="mySlider"
     pager="true"
     :options="slideOpts"
     @ionSlideDidChange="slideChanged"
   >
-    <ion-slide>
-      <span>Juli 2020</span>
-    </ion-slide>
-    <ion-slide>
-      <span>August 2020</span>
-    </ion-slide>
-    <ion-slide>
-      <span>September 2020</span>
+    <ion-slide v-for="month in sliderMonths" :key="month.key">
+      <span>{{ month.month }}</span>
     </ion-slide>
   </ion-slides>
-  
-  <ion-button @click="prevSlide" :disabled="disablePrevBtn">
-    <ion-icon slot="icon-only" :icon="chevronBack"></ion-icon>
-  </ion-button>  
+
   <ion-button @click="nextSlide" :disabled="disableNextBtn">
     <ion-icon slot="icon-only" :icon="chevronForward"></ion-icon>
   </ion-button>
-
 </template>
 
 <script>
@@ -30,7 +24,6 @@ import { IonIcon, IonSlides, IonSlide, IonButton } from "@ionic/vue";
 import { chevronBack, chevronForward } from "ionicons/icons";
 
 export default {
-
   components: {
     IonIcon,
     IonSlides,
@@ -49,6 +42,13 @@ export default {
       },
       disablePrevBtn: true,
       disableNextBtn: false,
+
+      sliderMonths: [
+        { key: 0, month: "Juli 2020" },
+        { key: 1, month: "August 2020" },
+        { key: 2, month: "September 2020" },
+        { key: 3, month: "Oktober 2020" },
+      ],
     };
   },
 
@@ -62,21 +62,16 @@ export default {
       await s.slideNext();
     },
     async slideChanged() {
-      console.log("slide changed 2");
-
       const s = await this.$refs.mySlider.$el.getSwiper();
 
       const sliderLength = s.slides.length;
       const activeSlide = s.activeIndex;
 
       this.disablePrevBtn = activeSlide === 0;
-      this.disableNextBtn = activeSlide === sliderLength-1;
+      this.disableNextBtn = activeSlide === sliderLength - 1;
     },
   },
-
 };
-
-
 </script>
 
 <style scoped>
